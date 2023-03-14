65bd10c24ae4b8dee3f97647dba0882bd3c41e50
PanzerWeb <selwynvillar@gmail.com>
2023-03-14 16:08:37 +0800

    Dark mode fix

--- app.js
+++ app.js
@@ -1,40 +1,35 @@
 //Dark Theme Toggler code from INSTAGRAM
-let icon = document.getElementById('icon');
 const body = document.querySelector('body');
+const icon = document.getElementById('icon');
 const initialTheme = 'light';
 
 const setTheme = (theme) => {
     localStorage.setItem('theme', theme);
-    body.setAttribute('data-theme', theme)
-
+    body.setAttribute('data-theme', theme);
 }
 
 const toggleTheme = () => {
     const activeTheme = localStorage.getItem('theme');
-    document.getElementById("toggleDay").style.background = "none";
-    icon.className = 'bi-moon-stars-fill text-light';
+    const isDarkMode = activeTheme === 'dark';
 
-
-    if (activeTheme === 'light') {
-    
-    
-    setTheme('dark');
-}
-    else{ 
+    if (isDarkMode) {
         setTheme('light');
-        document.getElementById("toggleDay").style.background = "yellow";
         icon.className = 'bi-brightness-high-fill';
-}
+        document.getElementById("toggleDay").style.background = "yellow";
+    } else { 
+        setTheme('dark');
+        icon.className = 'bi-moon-stars-fill text-light';
+        document.getElementById("toggleDay").style.background = "none";
+    }
 }
 
 const setThemeOnInit = () => {
     const savedTheme = localStorage.getItem('theme');
-    savedTheme
-        ? body.setAttribute('data-theme', savedTheme)
-        : setTheme(initialTheme);
-    
-        
+    savedTheme ? body.setAttribute('data-theme', savedTheme) : setTheme(initialTheme);
 }
+
+setThemeOnInit();
+
 
 
 
